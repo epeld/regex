fd1f66407a024be8eabd50e5ead2a0e904d6c6a3
Erik Peldan <epe@trifork.com>
2014-10-26 12:10:49 +0100

    Commit failing code

--- src/Compile.hs
+++ src/Compile.hs
@@ -1,21 +1,30 @@
 module Compile where
+import Data.Traversable
+import Control.Monad
+
+import State
 
 type Regex = String
 
-data CompiledRegex = CompiledRegex State
-data CompilationError = CompilationError
+data CompiledRegex = CompiledRegex [State]
+data CompilationError = InvalidEscape | CompilationError
 
-data State = MatchingState | IntermediateState Condition State (Maybe State)
-
-data Condition = Condition
 
 compile :: Regex -> Either CompilationError CompiledRegex 
-compile [] = Right $ CompiledRegex MatchingState
-compile s = Left CompilationError
+compile rx = liftM CompiledRegex $ compile_ rx
+
+compile_ :: Regex -> Either CompilationError [State]
+compile_ "" = Right []
+compile_ ('\\':xs) = case xs of
+    (x : xs') -> stateWithCharacter x : compile_ xs
+    [] -> Left InvalidEscape
+
+compile_ s = Left CompilationError
 
 match :: CompiledRegex -> String -> Maybe String
 match (CompiledRegex s) = match_ s
 
-match_ :: State -> String -> Maybe String
-match_ MatchingState _ = Just ""
+match_ :: [State] -> String -> Maybe String
+match_ [] _ = Just ""
 match_ s _ = Nothing
+
